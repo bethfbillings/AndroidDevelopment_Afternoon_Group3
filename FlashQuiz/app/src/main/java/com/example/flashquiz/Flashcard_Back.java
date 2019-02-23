2f34e1d869bb676be5a6bd52b2e63e4bc6162325
bethfbillings <42815574+bethfbillings@users.noreply.github.com>
2019-02-23 13:36:21 -0600

    Created AndroidDevelopment project and empty activities.

---
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/Flashcard_Back.java
@@ -0,0 +1,13 @@
+package com.example.flashquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flashcard_Back extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard__back);
+    }
+}
---
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/Flashcard_Front.java
@@ -0,0 +1,13 @@
+package com.example.flashquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flashcard_Front extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard__front);
+    }
+}
