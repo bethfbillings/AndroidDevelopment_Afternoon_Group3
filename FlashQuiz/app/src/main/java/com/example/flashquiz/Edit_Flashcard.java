1029824b690f14e0ba982702ec0c2266c8aa4588
bethfbillings <42815574+bethfbillings@users.noreply.github.com>
2019-03-11 22:14:49 -0500

    Edit nav

--- FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
@@ -1,7 +1,9 @@
 package com.example.flashquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Edit_Flashcard extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Edit_Flashcard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__flashcard);
+    }
+
+    public void goHome(View v) {
+        Intent goHome = new Intent(Edit_Flashcard.this, MainActivity.class);
+        startActivity(goHome);
     }
 }
