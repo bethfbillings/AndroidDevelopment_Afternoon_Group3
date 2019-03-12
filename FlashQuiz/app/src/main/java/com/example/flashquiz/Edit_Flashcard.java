88456495bb1e499219f7194437691c5fb8e05264
Simi-Maithani <44626074+Simi-Maithani@users.noreply.github.com>
2019-03-11 20:14:08 -0500

    Re-uploading zip file stuff.

--- FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
@@ -1,9 +1,7 @@
 package com.example.flashquiz;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class Edit_Flashcard extends AppCompatActivity {
 
@@ -11,10 +9,5 @@ public class Edit_Flashcard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__flashcard);
-    }
-
-    public void homeButton(View v) {
-        Intent goHome = new Intent(Edit_Flashcard.this, MainActivity.class);
-        Edit_Flashcard.this.startActivity(goHome);
     }
 }
--- FlashQuiz/app/src/main/java/com/example/flashquiz/MainActivity.java
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/MainActivity.java
@@ -4,29 +4,53 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+private Button Start;
+    private Button Stats;
+    private Button editflashcards;
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Start = (Button) findViewById(R.id.startBTN);
+        Start.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                openfront();
+            }
+        });
+
+        Stats = (Button) findViewById(R.id.statisticsBTN);
+        Stats.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                open_stats();
+            }
+        });
+        editflashcards = (Button) findViewById(R.id.editFlashcardsBTN);
+        editflashcards.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                open_editflash();
+            }
+        });
+
     }
-
-    /*public void startButton(View v) {
-        Intent start = new Intent(MainActivity.this, Flashcard_Front.class);
-        MainActivity.this.startActivity(start);
+    public void openfront() {
+        Intent front = new Intent(this, Flashcard_Front.class);
+        startActivityForResult(front, 1);
+        startActivity(front);
     }
-
-    public void statisticsButton(View v) {
-        Intent goStats = new Intent(MainActivity.this, Statistics.class);
-        MainActivity.this.startActivity(goStats);
+    public void open_stats() {
+        Intent stats = new Intent(this, Statistics.class);
+        startActivityForResult(stats, 1);
+        startActivity(stats);
     }
-
-    public void editFlashcardButton(View v) {
-        Intent goEdit = new Intent(MainActivity.this, Edit_Flashcard.class);
-        MainActivity.this.startActivity(goEdit);
-    }*/
-
+    public void open_editflash() {
+        Intent editflash = new Intent(this, Edit_Flashcard.class);
+        startActivityForResult(editflash, 1);
+        startActivity(editflash);
+    }
 
 }
