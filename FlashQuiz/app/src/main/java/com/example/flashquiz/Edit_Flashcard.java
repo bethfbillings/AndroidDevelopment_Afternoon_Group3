1c3a25745c965d09d8cfd59f01858024662a8115
bethfbillings <42815574+bethfbillings@users.noreply.github.com>
2019-04-08 22:52:50 -0500

    Added func to edit_flashcard

--- FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
@@ -4,17 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.Stack;
 
 public class Edit_Flashcard extends AppCompatActivity {
+
+    Stack<Flashcard> flashcards = new Stack<>();
+    EditText frontET;
+    EditText backET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit__flashcard);
+
+        frontET = (EditText) findViewById(R.id.editFrontET);
+        backET = (EditText) findViewById(R.id.editBackET);
     }
 
-    public void goHome(View v) {
+
+        public void goHome(View v) {
         Intent goHome = new Intent(Edit_Flashcard.this, MainActivity.class);
         startActivity(goHome);
     }
+
+    public void newCard(View v) {
+        Flashcard card = new Flashcard(frontET.getText().toString(), backET.getText().toString());
+        flashcards.add(card);
+    }
+
 }
