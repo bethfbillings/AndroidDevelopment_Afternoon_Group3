f9cd2ea43592ba0c735a911caef3e10465b8bf0e
bethfbillings <42815574+bethfbillings@users.noreply.github.com>
2019-04-21 20:18:26 -0500

    Fixed merge on edit flashcard

--- FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/Edit_Flashcard.java
@@ -11,17 +11,11 @@ import java.util.Stack;
 
 public class Edit_Flashcard extends AppCompatActivity {
 
-<<<<<<< HEAD
     EditText frontET;
     EditText backET;
     FlashcardsStack fqs;
     Stack<Flashcard> flashcards;
-    FlashQuizWrapper fqDB;
-=======
-    Stack<Flashcard> flashcards = new Stack<>();
-    EditText frontET;
-    EditText backET;
->>>>>>> c8e16e43cfaddd36070cbf07a5648bdce2187c20
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +24,8 @@ public class Edit_Flashcard extends AppCompatActivity {
 
         frontET = (EditText) findViewById(R.id.editFrontET);
         backET = (EditText) findViewById(R.id.editBackET);
-<<<<<<< HEAD
         fqs = (FlashcardsStack)getApplication();
         flashcards = fqs.getFlashcards();
-        fqDB = new FlashQuizWrapper(this);
-=======
->>>>>>> c8e16e43cfaddd36070cbf07a5648bdce2187c20
     }
 
 
@@ -45,22 +35,9 @@ public class Edit_Flashcard extends AppCompatActivity {
     }
 
     public void newCard(View v) {
-<<<<<<< HEAD
         Flashcard card = new Flashcard(frontET.getText().toString(), backET.getText().toString(), 0, 0);
         flashcards.add(card);
-        fqDB.insert(card);
         Toast.makeText(getApplicationContext(),"Flashcard Created",Toast.LENGTH_SHORT).show();
     }
 
-    public void saveFlashcards(View v) {
-        
-    }
-
-=======
-        Flashcard card = new Flashcard(frontET.getText().toString(), backET.getText().toString());
-        flashcards.add(card);
-        Toast.makeText(getApplicationContext(),"Flashcard Created",Toast.LENGTH_SHORT).show();
-    }
-
->>>>>>> c8e16e43cfaddd36070cbf07a5648bdce2187c20
 }
