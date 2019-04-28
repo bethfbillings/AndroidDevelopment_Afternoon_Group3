92f36427adf95bbeb07800e9fbbee4bcb1014261
Simi-Maithani <44626074+Simi-Maithani@users.noreply.github.com>
2019-04-28 18:35:47 -0500

    Flashcard_Front updated with the flashcards

--- FlashQuiz/app/src/main/java/com/example/flashquiz/Flashcard_Front.java
+++ FlashQuiz/app/src/main/java/com/example/flashquiz/Flashcard_Front.java
@@ -1,19 +1,32 @@
 package com.example.flashquiz;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Flashcard_Front extends AppCompatActivity {
 private Button check;
-    private Button home;
+private Button home;
+FQS fqs;
+Spinner s;
+public SharedPreferences preferences;
+String value;
+TextView front;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard__front);
-
+        preferences = getSharedPreferences("STACK", MODE_PRIVATE);
+        String value = preferences.getString("key", "");
+        fqs = new FQS(value);
+        front = (TextView) findViewById(R.id.frontTV);
+        front.setText(fqs.viewFront());
         check = (Button) findViewById(R.id.checkAnswerBTN);
         check.setOnClickListener(new View.OnClickListener() {
             public void onClick (View v){
@@ -27,6 +40,8 @@ private Button check;
             }
         });
     }
+
+
 
     public void openback(View view) {
         Intent back = new Intent(this, Flashcard_Back.class);
